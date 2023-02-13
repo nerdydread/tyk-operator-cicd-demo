60276fc280cb4d734529cc3d02d625f25e7531f4
Long Le <tyk-longle@Tyk-longle-MacBook-Pro.local>
2023-02-13 08:24:56 -0500

    added a cleanup script to dynamic delete apis and policies:

---
+++ clean-up.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Retrieve all yaml files
+files=(crds/*)
+
+curr_apis=()
+
+# Retrieve apis
+apis=( $(kubectl get tykapis -o json | jq -r '.items[].metadata.name') )
+
+# Retrieve policies
+policies=( $(kubectl get tykpolicies -o json | jq -r '.items[].metadata.name') )
+
+
+for file in "${files[@]}"; do
+    # Check if the file is a yaml file
+    if [[ $file == *.yaml ]]
+    then
+        echo $file -o json
+        # # Remove prefix and suffix
+        # prefix="crds/"
+        # suffix=".yaml"
+        # filename=${file#"$prefix"}
+        # filename=${filename%"$suffix"}
+
+        # curr_apis+=($filename)
+    fi
+done
+
+for api in "${curr_apis[@]}"; do
+    echo $api
+done
+
+for api in "${apis[@]}"; do
+    echo $api
+done
+
+for api in "${policies[@]}"; do
+    echo $api
+done
+
+# # Clean up tykapis
+# echo "Cleaning up tykapis..."
+# for api in "${apis[@]}"; do
+#     echo "$api"
+#     # Check if the value exists in the source of truth
+#     if [[ " ${file[*]} " =~ ${api} * ]]; 
+#         then
+#             # whatever you want to do when array contains value
+#             echo "FOUND"
+#         else
+#             echo "NOT FOUND"
+#     fi
+# done
+
+# echo "Cleaning up tykpolicies..."
+# for policy in "${policies[@]}"; do
+#     echo "$policy"
+# done
+
