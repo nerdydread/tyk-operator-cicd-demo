2a83723e02b754d196548ddc90591fa5d70b2b09
Long Le <tyk-longle@Tyk-longle-MacBook-Pro.local>
2023-02-13 10:34:36 -0500

    finished script to clean up tykapis and tykpolicies

--- clean-up.sh
+++ clean-up.sh
@@ -3,7 +3,8 @@
 # Retrieve all yaml files
 files=(crds/*)
 
-curr_apis=()
+# Current deployed apis and policies
+curr_apis_policies=()
 
 # Retrieve apis
 apis=( $(kubectl get tykapis -o json | jq -r '.items[].metadata.name') )
@@ -11,50 +12,41 @@ apis=( $(kubectl get tykapis -o json | jq -r '.items[].metadata.name') )
 # Retrieve policies
 policies=( $(kubectl get tykpolicies -o json | jq -r '.items[].metadata.name') )
 
-
+# Extract source of truth api and policies
 for file in "${files[@]}"; do
     # Check if the file is a yaml file
     if [[ $file == *.yaml ]]
     then
-        echo $file -o json
-        # # Remove prefix and suffix
-        # prefix="crds/"
-        # suffix=".yaml"
-        # filename=${file#"$prefix"}
-        # filename=${filename%"$suffix"}
-
-        # curr_apis+=($filename)
+        # Retrieve all metadata.name from CRDs
+        filename=$(yq e '.metadata.name' $file)
+        curr_apis_policies+=($filename)
     fi
 done
 
-for api in "${curr_apis[@]}"; do
-    echo $api
-done
-
+# Clean up tyk apis to match source of truth
 for api in "${apis[@]}"; do
-    echo $api
+    echo "Syncing deployement with source of truth for $api"
+    # Check if the value exists in the source of truth
+    if [[ " ${curr_apis_policies[*]} " =~ " ${api} " ]]; 
+        then
+            # whatever you want to do when array contains value
+            echo "API FOUND"
+        else
+            echo "API NOT FOUND: Cleaning up api..."
+            $(kubectl get tykapis $api -o yaml | yq 'del(.metadata.finalizers[], .spec.contextRef[])')
+    fi
 done
 
-for api in "${policies[@]}"; do
-    echo $api
+# Clean up tyk policies to match source of truth
+for policy in "${policies[@]}"; do
+    echo "Syncing deployement with source of truth for $policy"
+    # Check if the value exists in the source of truth
+    if [[ " ${curr_apis_policies[*]} " =~ " ${policy} " ]]; 
+        then
+            # whatever you want to do when array contains value
+            echo "POLICY FOUND"
+        else
+            echo "POLICY NOT FOUND: Cleaning up policy..."
+            $(kubectl get tykpolicies $api -o yaml | yq 'del(.metadata.finalizers[], .spec.contextRef[])')
+    fi
 done
-
-# # Clean up tykapis
-# echo "Cleaning up tykapis..."
-# for api in "${apis[@]}"; do
-#     echo "$api"
-#     # Check if the value exists in the source of truth
-#     if [[ " ${file[*]} " =~ ${api} * ]]; 
-#         then
-#             # whatever you want to do when array contains value
-#             echo "FOUND"
-#         else
-#             echo "NOT FOUND"
-#     fi
-# done
-
-# echo "Cleaning up tykpolicies..."
-# for policy in "${policies[@]}"; do
-#     echo "$policy"
-# done
-
