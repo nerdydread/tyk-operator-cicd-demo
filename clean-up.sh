b097e20e5f80ba2891b5ba311c77e4027671ac37
Long Le <tyk-longle@Tyk-longle-MacBook-Pro.local>
2023-02-16 10:10:16 -0500

    added deletion function to clean-up script

--- clean-up.sh
+++ clean-up.sh
@@ -3,16 +3,16 @@
 # Retrieve all yaml files
 files=(crds/*)
 
-# Current deployed apis and policies
+# Tykapis and tykpolicies
 curr_apis_policies=()
 
-# Retrieve apis
+# Extract apis
 apis=( $(kubectl get tykapis -o json | jq -r '.items[].metadata.name') )
 
-# Retrieve policies
+# Extract policies
 policies=( $(kubectl get tykpolicies -o json | jq -r '.items[].metadata.name') )
 
-# Extract source of truth api and policies
+# Extract source of truth api and policies (github repo)
 for file in "${files[@]}"; do
     # Check if the file is a yaml file
     if [[ $file == *.yaml ]]
@@ -26,27 +26,31 @@ done
 # Clean up tyk apis to match source of truth
 for api in "${apis[@]}"; do
     echo "Syncing deployement with source of truth for $api"
-    # Check if the value exists in the source of truth
+    # Check if the api exists in the source of truth
     if [[ " ${curr_apis_policies[*]} " =~ " ${api} " ]]; 
         then
-            # whatever you want to do when array contains value
+            # API found in source of truth
             echo "API FOUND"
         else
+            # API not found in source of truth
             echo "API NOT FOUND: Cleaning up api..."
             $(kubectl get tykapis $api -o yaml | yq 'del(.metadata.finalizers[], .spec.contextRef[])')
+            $(kubectl delete tykapis $api)
     fi
 done
 
 # Clean up tyk policies to match source of truth
 for policy in "${policies[@]}"; do
     echo "Syncing deployement with source of truth for $policy"
-    # Check if the value exists in the source of truth
+    # Check if the policy exists in the source of truth
     if [[ " ${curr_apis_policies[*]} " =~ " ${policy} " ]]; 
         then
-            # whatever you want to do when array contains value
+            # Policy found in source of truth
             echo "POLICY FOUND"
         else
+            # Policy not found in source of truth
             echo "POLICY NOT FOUND: Cleaning up policy..."
             $(kubectl get tykpolicies $api -o yaml | yq 'del(.metadata.finalizers[], .spec.contextRef[])')
+            $(kubectl delete tykapis $api)
     fi
 done
