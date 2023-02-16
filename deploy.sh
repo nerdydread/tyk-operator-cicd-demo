9c5e305aa88638d3a916b9dc71fae0c32de9f11b
Long Le <tyk-longle@Tyk-longle-MBP.home>
2023-02-16 14:08:20 -0500

    fixed bug in validation script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 dir="./crds"
-validation=( $(kubectl apply -f crds --dry-run=server) )
+validation=$(kubectl apply -f crds --dry-run=server 2>&1)
 
-if [[  " ${validation[*]} " =~ " error* " ]]
-    then
-        echo ${validation}
-        exit 1
-        
-    else
-        kubectl apply -f crds/
-        kubectl get tykapis
-        kubectl get tykpolicies
+if [[ "$validation" == *"error"* ]]
+then
+    echo $validation
+    exit 1
+else
+    echo "Validation success: deploying APIs and Policies..."
+    kubectl apply -f crds/
+    kubectl get tykapis
+    kubectl get tykpolicies
 fi
