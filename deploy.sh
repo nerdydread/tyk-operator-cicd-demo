18a71a6157f67d3166829534f7c8fd4d9db60627
Long Le <tyk-longle@Tyk-longle-MBP.home>
2023-02-16 13:46:34 -0500

    fixed logic for validatiopn

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@
 dir="./crds"
 validation=( $(kubectl apply -f crds --dry-run=server) )
 
-if [[ " ${validation[*]} " =~ " *error* " ]]
+if [[  " ${validation[*]} " =~ " error* " ]]
     then
+        echo ${validation}
+        exit 1
+        
+    else
         kubectl apply -f crds/
         kubectl get tykapis
         kubectl get tykpolicies
-    else
-        echo ${validation}
-        exit 1
 fi
